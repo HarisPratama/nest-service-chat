d82b8f892abdeeab243cdbc0ef07fd6795d8c3aa
HarisPratama <hpratama324@gmail.com>
2024-05-06 15:21:03 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ChatController } from './app.controller';
+import { ChatService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Chat, ChatSchema } from './schemas/messages.schema';
+
+const dbcloud =
+  'mongodb+srv://haris:3ynawF1zbEPR74IK@cluster0.r8pel.mongodb.net/youapp?retryWrites=true&w=majority&appName=Cluster0';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(dbcloud),
+    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    ClientsModule.register([
+      {
+        name: 'CHAT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqp://guest:guest@13.215.190.90:5672',
+            // 'amqps://ynvyudss:NplTt26iNlrD9BJadzgZJkR3Akqirftn@armadillo.rmq.cloudamqp.com/ynvyudss',
+          ],
+          queue: 'chat_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ChatController],
+  providers: [ChatService],
+})
+export class ChatModule {}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { ChatModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  // const app = await NestFactory.create(ChatModule);
+  // await app.listen(3001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    ChatModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          'amqp://guest:guest@13.215.190.90:5672',
+          // 'amqps://ynvyudss:NplTt26iNlrD9BJadzgZJkR3Akqirftn@armadillo.rmq.cloudamqp.com/ynvyudss',
+        ],
+        queue: 'chat_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
+}
+bootstrap();
